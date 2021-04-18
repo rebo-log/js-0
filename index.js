3a2a4a1e052ed82214d2d391d93f4f470ffbba31
rebo-log <reboterjob@gmail.com>
2021-04-18 13:04:06 +0300

    intial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const a = prompt("Enter first number");
+const b = prompt("Enter second number");
+
+alert(a + b);
+
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+const sum = num1 + num2;
+alert(`The sum of ${num1} and ${num2} is ${sum}`);
