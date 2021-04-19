c54ac7356f09cba4d6c4db263f184a2f081a85ad
rebo-log <reboterjob@gmail.com>
2021-04-19 23:46:01 +0300

    fix NaN

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const a = prompt("Enter first number");
-const b = prompt("Enter second number");
+let num1 = parseInt(prompt('Enter the first number '));
+let num2 = parseInt(prompt('Enter the second number '));
 
-alert(a + b);
 
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
+num1 = num1 || 0;
+num2 = num2 || 0;
+
 const sum = num1 + num2;
-alert(`The sum of ${num1} and ${num2} is ${sum}`);
+
+alert ( `The sum of ${num1} and ${num2} is ${sum}` );
+
+
+
